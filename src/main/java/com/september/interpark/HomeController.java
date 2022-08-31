d6f87ad614dfcfb73933c95aad1631e32b000c3e
Park Seung Jun <Park Seung Jun@DESKTOP-OAAAUEK>
2022-08-31 15:29:46 +0900

    20220831 1529

--- src/main/java/com/september/interpark/HomeController.java
+++ src/main/java/com/september/interpark/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 		public String home(Locale locale, Model model) {
 			logger.info("Welcome home! The client locale is {}.", locale);
 			
-			Date date = new Date();
+			Date date = new Date(); /* 시간나오게 해주는것 */
 			DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 			
 			String formattedDate = dateFormat.format(date);
@@ -33,5 +33,51 @@ public class HomeController {
 			
 			return "home";
 		}
+		
+		@RequestMapping(value = "/a", method = RequestMethod.GET)
+		public String a(Locale locale, Model model) {
+			return "a";
+		}
+		
+		@RequestMapping(value = "/b", method = RequestMethod.GET)
+		public String b(Locale locale, Model model) {
+			return "b";
+		}
+		
+		@RequestMapping(value = "/c", method = RequestMethod.GET)
+		public String c(Locale locale, Model model) {
+			return "c";
+		}
+		
+		@RequestMapping(value = "/d", method = RequestMethod.GET)
+		public String d(Locale locale, Model model) {
+			return "d";
+		}
+		
+		@RequestMapping(value = "/e", method = RequestMethod.GET)
+		public String e(Locale locale, Model model) {
+			return "e";
+		}
+		
+		@RequestMapping(value = "/codeList", method = RequestMethod.GET)
+		public String codeList(Locale locale, Model model) {
+			return "codeList";
+		}
+		
+		@RequestMapping(value = "/codeForm", method = RequestMethod.GET)
+		public String codeForm(Locale locale, Model model) {
+			return "codeForm";
+		}
+		
+		@RequestMapping(value = "/codeGroupList", method = RequestMethod.GET)
+		public String codeGroupList(Locale locale, Model model) {
+			return "codeGroupList";
+		}
+		
+		@RequestMapping(value = "/codeGroupForm", method = RequestMethod.GET)
+		public String codeGroupForm(Locale locale, Model model) {
+			return "codeGroupForm";
+		}
+		
 		
 }
---
+++ src/main/java/com/september/interpark/modules/codegroup/CodeGroup.java
@@ -0,0 +1,50 @@
+package com.september.interpark.modules.codegroup;
+
+public class CodeGroup {
+
+//	infr_code_group
+	private String Seq;
+	private String SeqAnother;
+	private String ccgname;
+	private Integer ccguseNy;
+	private Integer ccgorder;
+//////////////////////////////////////////////////////////////////////
+	public String getSeq() {
+		return Seq;
+	}
+	public void setSeq(String seq) {
+		Seq = seq;
+	}
+	public String getSeqAnother() {
+		return SeqAnother;
+	}
+	public void setSeqAnother(String seqAnother) {
+		SeqAnother = seqAnother;
+	}
+	public String getCcgname() {
+		return ccgname;
+	}
+	public void setCcgname(String ccgname) {
+		this.ccgname = ccgname;
+	}
+	public Integer getCcguseNy() {
+		return ccguseNy;
+	}
+	public void setCcguseNy(Integer ccguseNy) {
+		this.ccguseNy = ccguseNy;
+	}
+	public Integer getCcgorder() {
+		return ccgorder;
+	}
+	public void setCcgorder(Integer ccgorder) {
+		this.ccgorder = ccgorder;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
