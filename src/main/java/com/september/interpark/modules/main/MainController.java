6f63b224d96268c8e1ca6dc141647563f66ca061
seungjun12 <vndan110@naver.com>
2022-09-29 08:57:59 +0900

    start

--- src/main/java/com/september/interpark/modules/main/MainController.java
+++ src/main/java/com/september/interpark/modules/main/MainController.java
@@ -1,11 +1,8 @@
 package com.september.interpark.modules.main;
 
-import java.util.Locale;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(value = "/main/")
