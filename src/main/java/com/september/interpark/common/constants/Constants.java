d8983985a22bc579c7b36d60b2f30dc6f9bf1cf8
seungjun12 <vndan110@naver.com>
2022-11-23 09:17:41 +0900

    상수 java파일 수정

--- src/main/java/com/september/interpark/common/constants/Constants.java
+++ src/main/java/com/september/interpark/common/constants/Constants.java
@@ -1,6 +1,6 @@
 package com.september.interpark.common.constants;
 
-public class Constants {
+public final class Constants {
 	public static final int ROW_NUM_TO_SHOW = 10;
 	public static final int PAGE_NUM_TO_SHOW = 5;
 	public static final int SESSION_MINUTE = 30;
